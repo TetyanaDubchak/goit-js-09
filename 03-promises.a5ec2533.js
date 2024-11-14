4e10c8ebf2462da8e90437412637fda0801bca39
TetyanaDubchak <123674629+TetyanaDubchak@users.noreply.github.com>
2024-11-14 15:18:34 +0000

    Deploying to gh-pages from @ TetyanaDubchak/goit-js-09@edfcb4aded15fb1e20b8d00e047f569959d184e4 🚀

---
+++ 03-promises.a5ec2533.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault,console.dir(e.target)}));
+//# sourceMappingURL=03-promises.a5ec2533.js.map
---
+++ 03-promises.bc931665.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(e=>{e.preventDefault,console.dir(e.target)}));
+//# sourceMappingURL=03-promises.bc931665.js.map

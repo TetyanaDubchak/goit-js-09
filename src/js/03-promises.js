14148c792775c7daf9249052cf4f428ec811d57e
TetyanaDubchak <123674629+TetyanaDubchak@users.noreply.github.com>
2024-11-15 19:46:45 +0200

    All tasks ready

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,17 +1,46 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  })
 }
 
 const formWrapper = document.querySelector('.form');
 
 const onSubmitHandle = (e) => {
-  e.preventDefault;
-  console.dir(e.target);
+  e.preventDefault();
+
+  let delayValue =Number(e.target.delay.value) ;
+  let stepValue = Number(e.target.step.value);
+  let amountValue = Number(e.target.amount.value);
+ 
+  let currentDelay = delayValue;
+
+  for (let index = 1; index <= amountValue; index ++) {
+
+      createPromise(index, currentDelay)
+        .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+    })
+        .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+        });
+      
+      currentDelay = delayValue + (index * stepValue);
+  }
+
+  e.target.delay.value = '';
+  e.target.step.value = '';
+  e.target.amount.value = '';
 };
 
 formWrapper.addEventListener('submit', onSubmitHandle)
